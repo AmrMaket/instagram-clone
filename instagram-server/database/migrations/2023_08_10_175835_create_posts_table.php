9f4911fa2a53a6dc4183d1bca656e9fba8aba5fd
Amr Maket <amrmaket89@gmail.com>
2023-08-10 21:33:01 +0300

    Server <> ficing routers

--- instagram-server/database/migrations/2023_08_10_175835_create_posts_table.php
+++ instagram-server/database/migrations/2023_08_10_175835_create_posts_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('user_is');
+            $table->unsignedBigInteger('user_id');
             $table->string('image_url');
             $table->timestamps();
         });
