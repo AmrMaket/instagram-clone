10b715ca3c4e5da49c6e2cc206c72771baf1a4bc
Amr Maket <amrmaket89@gmail.com>
2023-08-10 13:58:25 +0300

    Client <> images with styling

--- instagram-client/src/Components/Menu/index.jsx
+++ instagram-client/src/Components/Menu/index.jsx
@@ -1,33 +1,63 @@
 import React from 'react';
-import home from '../../assets/home.svg';
-import explore from '../../assets/explore.svg';
-import notifications from '../../assets/notifications.svg';
-import profile from '../../assets/profile1.png';
-import inbox from '../../assets/inbox.svg';
 import './style.css';
-
-
+import logo from '../../assets/instagram-text-logo-83656.png'
+import home from '../../assets/home.png';
+import search from '../../assets/search.png';
+import export1 from '../../assets/export.png';
+import reels from '../../assets/reels.png';
+import messages from '../../assets/messages.png';
+import notifications from '../../assets/notifications.png';
+import create from '../../assets/create.png';
+import profile from '../../assets/profile1.png';
+import more from '../../assets/more.png';
 
 
 const Menu = () => {
     return ( 
-        <div className='menu'>
-            <div className='icon'>
-                <img src={home} alt='#' className='bookmark-logo'/ >
+        <div className='container'>
+            <div className='menu-logo'>
+                <img src={logo}  alt= 'logo' />
             </div>
-            <div className='icon'>
-                <img src={inbox} alt='#' className='inbox-logo'/ >
-            </div>
-            <div className='icon'>
-                <img src={explore} alt='#' className='explore-logo'/ >
-            </div>
-            <div className='icon'>
-                <img src={notifications} alt='#' className='notifications-logo'/ >
-            </div>
-            <div className='icon'>
-                <img src={profile} alt='#' className='profileIcon'/ >
+            <div className='menu-body'>
+                <div className='menu-icon'>
+                    <img src={home}  alt='home' />
+                    <div>Home</div>
+                </div>
+                <div className='menu-icon'>
+                    <img src={search}  alt='search' />
+                    <div>Search</div>
+                </div>
+                <div className='menu-icon'>
+                    <img src={export1}  alt='explore' />
+                    <div>Export</div>
+                </div>
+                <div className='menu-icon'>
+                    <img src={reels}  alt='reels' />
+                    <div>Reels</div>
+                </div>
+                <div className='menu-icon'>
+                    <img src={messages}  alt='meassages' />
+                    <div>Messages</div>
+                </div>
+                <div className='menu-icon'>
+                    <img src={notifications}  alt='notifications' />
+                    <div>Notifications</div>
+                </div>
+                <div className='menu-icon'>
+                    <img src={create}  alt='create' />
+                    <div>Create</div>
+                </div>
+                <div className='menu-icon'>
+                    <img src={profile}  alt='profile' />
+                    <div>Profile</div>
+                </div>
+                <div className='menu-icon'>
+                    <img src={more}  alt='More' />
+                    <div>More</div>
+                </div>
             </div>
         </div>
      );
 }
-export default Menu ;
+ 
+export default Menu;
